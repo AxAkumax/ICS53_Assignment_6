2ef8c255d80620d5f900b9735c302bd9b06c127e
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-12-11 00:16:46 -0800

    some comment changes

--- hw6.c
+++ hw6.c
@@ -6,14 +6,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MAX_BUFFER_SIZE 10
-#define MAX_THREADS 16
+const int MAX_BUFFER_SIZE = 10;
+const int MAX_THREADS = 16;
 
 pthread_mutex_t buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t buffer_not_full = PTHREAD_COND_INITIALIZER;
 pthread_cond_t buffer_not_empty = PTHREAD_COND_INITIALIZER;
 
-int total_producers, total_consumers, items_per_producer, buffer_size, delay;
+int total_producers;
+int total_consumers;
+int items_per_producer;
+int buffer_size, delay;
 
 int buffer[MAX_BUFFER_SIZE];
 int buffer_head = 0;
@@ -23,28 +26,28 @@ void* producer(void* arg) {
     int producer_id = *(int*)arg;
 
     for (int i = 0; i < items_per_producer; ++i) {
-        // Produce item
+        //produce item
         int item = producer_id * items_per_producer + i;
 
         pthread_mutex_lock(&buffer_mutex);
 
-        // Wait if buffer is full
+        //wait if buffer is full
         while ((buffer_tail + 1) % buffer_size == buffer_head) {
             //mutex is unlocked since we need a consumer to consume the item since the buffer is full
             pthread_cond_wait(&buffer_not_full, &buffer_mutex);
         }
 
         buffer[buffer_tail] = item;
-        //add the tail (pointer) to move onto the next slot to produced
+        //add the tail to move onto the next slot to produced
         buffer_tail = (buffer_tail + 1) % buffer_size;
 
-        // Print produced item
+        //print produced item
         printf("producer_%d produced item %d\n", producer_id, item);
 
         pthread_cond_signal(&buffer_not_empty);
         pthread_mutex_unlock(&buffer_mutex);
 
-        // Introduce delay if needed
+        //introduce delay if needed
         if (delay == 1) {
             usleep(500000); //delay of 500,000 microseconds
         }
@@ -59,18 +62,18 @@ void* consumer(void* arg) {
     for (int i = 0; i < items_per_producer * total_producers / total_consumers; ++i) {
         pthread_mutex_lock(&buffer_mutex);
 
-        // Wait if buffer is empty
+        //wait if buffer is empty
         while (buffer_head == buffer_tail) {
             //mutex is unlocked since we need a producer thread to produce an item in order to consume
             pthread_cond_wait(&buffer_not_empty, &buffer_mutex);
         }
 
-        // Consume item
+        //consume item
         int item = buffer[buffer_head];
-        //add the head (pointer) to move onto the next slot to be consumed
+        //add the head to move onto the next slot to be consumed
         buffer_head = (buffer_head + 1) % buffer_size;
 
-        // Print consumed item
+        //print consumed item
         printf("consumer_%d consumed item %d\n", consumer_id, item);
 
         pthread_cond_signal(&buffer_not_full);
@@ -85,11 +88,6 @@ void* consumer(void* arg) {
     return NULL;
 }
 int main(int argc, char* argv[]) {
-    if (argc != 6) {
-        fprintf(stderr, "Usage: %s p c i b d\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
     total_producers = atoi(argv[1]);
     total_consumers = atoi(argv[2]);
     items_per_producer = atoi(argv[3]);
@@ -101,28 +99,30 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    //although initialized to MAX_THREADS we will only utilize the arguments for each of the data structures
-    pthread_t producers[MAX_THREADS], consumers[MAX_THREADS];
-    int producer_ids[MAX_THREADS], consumer_ids[MAX_THREADS]; 
+    //although initialized to MAX_THREADS we will only use the arguments for each of the data structures
+    pthread_t producers[MAX_THREADS];
+    pthread_t consumers[MAX_THREADS];
+    int producer_ids[MAX_THREADS];
+    int consumer_ids[MAX_THREADS]; 
 
-    // Creating the producer threads
+    //create producer threads
     for (int i = 0; i < total_producers; ++i) {
         producer_ids[i] = i;
         pthread_create(&producers[i], NULL, producer, &producer_ids[i]);
     }
 
-    // Creating the consumer threads
+    //create consumer threads
     for (int i = 0; i < total_consumers; ++i) {
         consumer_ids[i] = i;
         pthread_create(&consumers[i], NULL, consumer, &consumer_ids[i]);
     }
 
-    // Wait for all producer threads to finish (threads are non-detached hence join is used)
+    //wait for all producer threads to finish (threads are non-detached so join is used)
     for (int i = 0; i < total_producers; ++i) {
         pthread_join(producers[i], NULL);
     }
 
-    // Wait for all consumer threads to finish
+    //wait for all consumer threads to finish
     for (int i = 0; i < total_consumers; ++i) {
         pthread_join(consumers[i], NULL);
     }
